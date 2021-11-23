ab200dcf2167eaf61dd1ea44494e9c387369056b
mohammad-fazeli <moohammadfazeli@gmail.com>
2021-11-23 22:21:14 +0330

    update DropDown.js

--- src/components/Header/components/DropDown.js
+++ src/components/Header/components/DropDown.js
@@ -9,7 +9,7 @@ const DropDown = ({ title = "", items = [] }) => {
           <DropDownItem
             key={index}
             name={item.name}
-            subvision={item.subdivision}
+            subvision={item.subvision}
             address={item.address}
           />
         ))}
